3cb9a7e9cb2de401784fae720165b5bfae96371e
Thejus Paul <thejuspaul@pm.me>
2025-04-16 01:27:06 +0530

    Enabled strict loading violation

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,7 @@ module DramaSpy
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_record.action_on_strict_loading_violation = :raise
   end
 end
