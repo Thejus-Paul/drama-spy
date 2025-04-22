6cbabbccd618dccee68d43b856148ec72d142242
Thejus Paul <thejuspaul@pm.me>
2025-04-22 11:36:04 +0530

    Added the model validations and its test cases

--- app/models/drama.rb
+++ app/models/drama.rb
@@ -1,21 +1,41 @@
 # Drama model with required validations
 class Drama < ApplicationRecord
+  MAX_DESCRIPTION_LENGTH = 1000
+  MAX_COUNTRY_LENGTH = 50
+  MAX_NAME_LENGTH = 100
+  MAX_EPISODE_COUNT = 200
+  MIN_EPISODE_COUNT = 1
+  MIN_LAST_WATCHED_EPISODE = 0
+
   enum :airing_status, [ :upcoming, :ongoing, :completed ], default: :upcoming, validate: true
   enum :watch_status, [ :not_started, :watching, :finished ], default: :not_started, validate: true
 
-  validates :airing_status, :country, :name, :watch_status, presence: true
-  validates :name, uniqueness: true
-  validates :last_watched_episode, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: :total_episodes }
-  validates :total_episodes, presence: true, numericality: { greater_than_or_equal_to: 1 }
-  validate :last_watched_episode_valid
+  validates :airing_status, :country, :name, :watch_status, :total_episodes, :last_watched_episode, presence: true
+  validates :description, length: { maximum: MAX_DESCRIPTION_LENGTH }
+  validates :country, length: { maximum: MAX_COUNTRY_LENGTH }
+  validates :name, length: { maximum: MAX_NAME_LENGTH }, uniqueness: true
+  validates :last_watched_episode, numericality: { greater_than_or_equal_to: MIN_LAST_WATCHED_EPISODE, less_than_or_equal_to: :total_episodes }
+  validates :total_episodes, numericality: { only_integer: true, greater_than_or_equal_to: MIN_EPISODE_COUNT, less_than_or_equal_to: MAX_EPISODE_COUNT }
+
+  validate :last_watched_episode_valid, :total_episodes_valid
 
   before_save :update_watch_status
 
   private
 
   def last_watched_episode_valid
-    if last_watched_episode.present? && total_episodes.present? && last_watched_episode > total_episodes
-      errors.add(:last_watched_episode, "cannot exceed total episodes")
+    return unless last_watched_episode.present? && total_episodes.present?
+
+    if last_watched_episode < 0 || last_watched_episode > total_episodes
+      errors.add(:last_watched_episode, "must be between 0 and the total number of episodes")
+    end
+  end
+
+  def total_episodes_valid
+    return unless total_episodes.present?
+
+    if total_episodes < 1 || total_episodes > 200
+      errors.add(:last_watched_episode, "must be a valid number between 1 and 200")
     end
   end
 
--- test/models/drama_test.rb
+++ test/models/drama_test.rb
@@ -7,12 +7,13 @@ class DramaTest < ActiveSupport::TestCase
   end
 
   test "should be valid with valid attributes" do
-    assert @drama.valid?
+    assert_predicate(@drama, :valid?)
   end
 
   test "should require presence of airing_status" do
     @drama.airing_status = nil
-    refute @drama.valid?
+
+    refute_predicate(@drama, :valid?)
     assert_includes(@drama.errors.full_messages, "Airing status can't be blank")
   end
 
@@ -20,62 +21,93 @@ class DramaTest < ActiveSupport::TestCase
     assert_presence_validation(:country, "Country can't be blank")
   end
 
+  test "should enforce country length limit" do
+    @drama.country = "A" * (::Drama::MAX_COUNTRY_LENGTH + 1)
+
+    refute_predicate(@drama, :valid?)
+    assert_equal("Country is too long (maximum is #{::Drama::MAX_COUNTRY_LENGTH} characters)", @drama.errors.full_messages.to_sentence)
+  end
+
   test "should require presence of name" do
     assert_presence_validation(:name, "Name can't be blank")
   end
 
+  test "should enforce name length limit" do
+    @drama.name = "A" * (::Drama::MAX_NAME_LENGTH + 1)
+
+    refute_predicate(@drama, :valid?)
+    assert_equal("Name is too long (maximum is #{::Drama::MAX_NAME_LENGTH} characters)", @drama.errors.full_messages.to_sentence)
+  end
+
+  test "should enforce description length limit" do
+    @drama.description = "A" * (::Drama::MAX_DESCRIPTION_LENGTH + 1)
+
+    refute_predicate(@drama, :valid?)
+    assert_equal("Description is too long (maximum is #{::Drama::MAX_DESCRIPTION_LENGTH} characters)", @drama.errors.full_messages.to_sentence)
+  end
+
   test "should require presence of watch_status" do
     @drama.watch_status = nil
-    refute @drama.valid?
+
+    refute_predicate(@drama, :valid?)
     assert_includes(@drama.errors.full_messages, "Watch status can't be blank")
   end
 
   test "should enforce uniqueness of name" do
     duplicate_drama = @drama.dup
-    refute duplicate_drama.valid?
+
+    refute_predicate(duplicate_drama, :valid?)
     assert_equal("Name has already been taken", duplicate_drama.errors.full_messages.to_sentence)
   end
 
   test "should be invalid when last_watched_episode is negative" do
     @drama.last_watched_episode = -1
-    refute @drama.valid?
+
+    refute_predicate(@drama, :valid?)
   end
 
   test "should be invalid when last_watched_episode exceeds total episodes" do
     @drama.last_watched_episode = @drama.total_episodes + 1
-    refute @drama.valid?
+
+    refute_predicate(@drama, :valid?)
   end
 
   test "should be valid when last_watched_episode is within range" do
     @drama.last_watched_episode = @drama.total_episodes
-    assert @drama.valid?
+
+    assert_predicate(@drama, :valid?)
   end
 
   test "should be invalid when total_episodes is zero" do
     @drama.total_episodes = 0
-    refute @drama.valid?
+
+    refute_predicate(@drama, :valid?)
   end
 
   test "should be valid when total_episodes is positive" do
     @drama.total_episodes = 1
-    assert @drama.valid?
+
+    assert_predicate(@drama, :valid?)
   end
 
   test "should set watch_status to not_started when last_watched_episode is 0" do
     @drama.last_watched_episode = 0
     @drama.save!
+
     assert_equal("not_started", @drama.watch_status)
   end
 
   test "should set watch_status to watching when last_watched_episode is in progress" do
     @drama.last_watched_episode = @drama.total_episodes - 1
     @drama.save!
+
     assert_equal("watching", @drama.watch_status)
   end
 
   test "should set watch_status to finished when last_watched_episode equals total_episodes" do
     @drama.last_watched_episode = @drama.total_episodes
     @drama.save!
+
     assert_equal("finished", @drama.watch_status)
   end
 
@@ -83,7 +115,8 @@ class DramaTest < ActiveSupport::TestCase
 
   def assert_presence_validation(attribute, expected_message)
     @drama.send("#{attribute}=", nil)
-    refute @drama.valid?
+
+    refute_predicate(@drama, :valid?)
     assert_equal(expected_message, @drama.errors.full_messages.to_sentence)
   end
 end
