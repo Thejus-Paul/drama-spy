63ddcecc1f371450f4d3e4e69e775251827ff5ab
Thejus Paul <thejuspaul@pm.me>
2025-06-06 08:56:05 +0000

    Updated to create or update drama from create method (#42)
    * Updated the dependency gems
    
    * Updated to create or update drama on create method

--- app/controllers/api/v1/dramas_controller.rb
+++ app/controllers/api/v1/dramas_controller.rb
@@ -2,6 +2,7 @@
 class Api::V1::DramasController < ApplicationController
   DRAMA_LIST_CACHE_KEY = "drama_list"
 
+  before_action :find_drama, only: :create
   before_action :load_drama, only: :update
   before_action -> { Rails.cache.delete(DRAMA_LIST_CACHE_KEY) }, only: %i[create update]
 
@@ -12,11 +13,10 @@ class Api::V1::DramasController < ApplicationController
   end
 
   def create
-    drama = Drama.new(drama_params)
-    if drama.valid?
-      render(status: :ok, json: { "notice": "Created!" })
+    if @drama.update(drama_params)
+      render(status: :ok, json: { "notice": @success_message })
     else
-      render(status: :unprocessable_entity, json: { "error": drama.errors.full_messages.to_sentence.capitalize })
+      render(status: :unprocessable_entity, json: { "error": @drama.errors.full_messages.to_sentence.capitalize })
     end
   end
 
@@ -29,6 +29,12 @@ class Api::V1::DramasController < ApplicationController
   end
 
   private
+
+  def find_drama
+    @drama = Drama.find_or_initialize_by(name: drama_params[:name])
+    @success_message = @drama.new_record? ? "Created!" : "Updated!"
+  end
+
   def load_drama
     @drama = Drama.find_by!(name: drama_params[:name])
   end
