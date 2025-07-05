5dc4e10ce73fdb7f092e7721a2661a6fe4bb00a0
Thejus Paul <thejuspaul@pm.me>
2025-07-05 18:53:41 +0530

    Added Typerlizer DSL helper

--- app/resources/drama/index_resource.rb
+++ app/resources/drama/index_resource.rb
@@ -1,4 +1,5 @@
 # Used to serialize a list of dramas into a JSON response for index action.
 class Drama::IndexResource < ApplicationResource
+  typelize_from Drama
   attributes(:id, :last_watched_episode, :name, :watch_status)
 end
--- app/resources/drama/show_resource.rb
+++ app/resources/drama/show_resource.rb
@@ -2,5 +2,6 @@
 class Drama::ShowResource < ApplicationResource
   attribute(:status) { :success }
 
+  typelize_from Drama
   attributes(:airing_status, :country, :description, :id, :last_watched_episode, :name, :total_episodes, :watch_status)
 end
