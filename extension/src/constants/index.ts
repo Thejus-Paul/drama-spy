d3b11e45a64272b11c9692b71ba4304b50295734
Thejus Paul <thejuspaul@pm.me>
2025-07-20 23:37:15 +0530

    Fixed the retry attempt count

--- extension/src/constants/index.ts
+++ extension/src/constants/index.ts
@@ -3,7 +3,7 @@ export const APP_URL: string = "https://drama-spy.neetodeployapp.com";
 export const API_V1_URL: string = `${APP_URL}/api/v1`;
 
 // Total 5 attempts as initial attempt + 4 retries
-export const RETRY_ATTEMPTS: number = 5;
+export const RETRY_ATTEMPTS: number = 4;
 
 // 1 second offset for Math.log2 otherwise it will trigger the first retry immediately
 export const OFFSET: number = 1;
