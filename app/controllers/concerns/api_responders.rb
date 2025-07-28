274d4f65c0cf4f4902a623ec63c392c996c1aa38
Thejus Paul <thejuspaul@pm.me>
2025-07-29 02:07:42 +0530

    Refactored API responders

--- app/controllers/concerns/api_responders.rb
+++ app/controllers/concerns/api_responders.rb
@@ -1,8 +1,12 @@
 # Used to render success and error responses in a consistent manner.
 module ApiResponders
-  def render_success(message, status: :ok) = render(status:, json: SuccessResource.new({ message: }).serialize)
+  def render_success(message, status: :ok) = render(
+    status:, json: SuccessResource.new({ message: }).serialize
+  )
 
-  def render_error(message, status: :unprocessable_entity) = render(status:, json: ErrorResource.new({ message: }).serialize)
+  def render_error(message, status: :unprocessable_entity) = render(
+    status:, json: ErrorResource.new({ message: }).serialize
+  )
 
   def render_json(resource, status: :ok) = render(status:, json: resource.serialize)
 end
