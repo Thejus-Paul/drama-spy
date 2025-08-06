0283f0f7dc296f1236ff0896ee0df3930bc2e711
Thejus Paul <thejuspaul@pm.me>
2025-08-07 00:14:57 +0530

    Added Bearer scan to ./bin/ci workflow

--- config/ci.rb
+++ config/ci.rb
@@ -7,6 +7,7 @@ CI.run do
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
+  step "Security: Bearer scan", "bearer scan --quiet --exit-code -1 --fail-on-severity critical,high,medium,low ."
   step "Tests: Rails", "bin/rails test"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
   step "Performance: ActiveRecord doctor", "bin/rails active_record_doctor"
