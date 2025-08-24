dff9e4f394802156c7efb5bc6b514a10aeb7bd1f
Thejus Paul <thejuspaul@pm.me>
2025-08-25 00:14:43 +0530

    Removed unused health check

--- extension/entrypoints/kk/index.ts
+++ extension/entrypoints/kk/index.ts
@@ -1,10 +1,7 @@
-import messaging from "../messaging";
 import dramaPage from "./dramaPage";
 import homePage from "./homePage";
 
 const router = (url: URL) => {
-  messaging.sendMessage("up");
-
   if (url.pathname.startsWith("/Drama/")) dramaPage();
   else if (url.pathname === "/") homePage();
 };
