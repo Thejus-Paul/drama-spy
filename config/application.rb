b98b7899cdbb9f2dc05e25857a0ea7f4699550fb
Thejus Paul <thejuspaul@pm.me>
2026-03-28 08:51:54 +0530

    refactor: remove Solid Queue and ActiveJob (no jobs exist)

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require_relative "boot"
 require "rails"
 
 require "active_model/railtie"
-require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "rails/test_unit/railtie" unless Rails.env.production?
